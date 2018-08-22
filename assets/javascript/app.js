1e50f9472edc1a3364ad17d63aa61ac9710b8b0c
Odin Almighty <36708782+Treybrewer@users.noreply.github.com>
2018-08-21 23:26:53 -0400

    adding table to html and background and styling to css

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBJIBkDahW7LZJ2SzlY1nfZwQ2H6iHJGD8",
+    authDomain: "database-project1-31fcc.firebaseapp.com",
+    databaseURL: "https://database-project1-31fcc.firebaseio.com",
+    projectId: "database-project1-31fcc",
+    storageBucket: "database-project1-31fcc.appspot.com",
+    messagingSenderId: "1086742283789"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
