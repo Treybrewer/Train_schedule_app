c643257247cce09304be30deef10fa9b777f1224
Odin Almighty <36708782+Treybrewer@users.noreply.github.com>
2018-08-23 22:57:20 -0400

    adding tables to html, creating basic logic setup to js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,21 @@
- // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBJIBkDahW7LZJ2SzlY1nfZwQ2H6iHJGD8",
-    authDomain: "database-project1-31fcc.firebaseapp.com",
-    databaseURL: "https://database-project1-31fcc.firebaseio.com",
-    projectId: "database-project1-31fcc",
-    storageBucket: "database-project1-31fcc.appspot.com",
-    messagingSenderId: "1086742283789"
-  };
-  firebase.initializeApp(config);
-  var database = firebase.database();
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBJIBkDahW7LZJ2SzlY1nfZwQ2H6iHJGD8",
+  authDomain: "database-project1-31fcc.firebaseapp.com",
+  databaseURL: "https://database-project1-31fcc.firebaseio.com",
+  projectId: "database-project1-31fcc",
+  storageBucket: "database-project1-31fcc.appspot.com",
+  messagingSenderId: "1086742283789"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+var appObj = {
+  submitButton: function (event) {
+    var submit = $("<button>");
+    submit.addClass("submitbutton");
+    submit.text('Submit');
+    $(".tbody").append(submit);
+
+  },
+  // submitButton()
+};
