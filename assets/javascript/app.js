de8b2a7108e24848bcd527434a1a31c370fbd024
Odin Almighty <36708782+Treybrewer@users.noreply.github.com>
2018-09-18 21:08:00 -0400

    .

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyBJIBkDahW7LZJ2SzlY1nfZwQ2H6iHJGD8",
@@ -30,8 +31,43 @@ var appObj = {
 
 
     });
+    database.ref('users/TrainApp/').once("value", function (snapshot) {
+      console.log(snapshot.val());
+      console.log(snapshot.val().trainName);
+      var newTrainName = snapshot.val().trainName;
+      var newDestination = snapshot.val().Destination;
+      var newFrequency = snapshot.val().Frequency;
+      var time = moment().add(newFrequency, "minutes");
+      var newTime = time.format('h:mm: a');
+      var tr = $("<tr class='table-row'>");
+      var th = $("<th scope = 'row'>")
+      var td = $("<td>");
+      var td2 = $("<td class='td2'>");
+      var td3 = $("<td class = 'td3'>");
+      var td4 = $("<td class = 'td4'>");
+      td.append(newDestination);
+      td2.append(newFrequency);
+      td3.append(newTime);
+      td4.append(time.fromNow());
+      th.append(newTrainName);
+      tr.append(th, td, td2, td3, td4);
+
+      $(".main-table").append(tr);
+
+
+
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
+  },
+  postTrain: function (event) {
+
   },
   submitButton: function (event) {
+    var time = moment().add(12, "minutes");
+    var newTime = time.format('h:mm: a');
+    $(".td3").append(newTime);
+    $(".td4").append(time.fromNow());
     var submit = $("<button>");
     submit.addClass("submitbutton");
     submit.text('Submit');
